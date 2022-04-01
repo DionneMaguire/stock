ae83e80ceee3dc7bed6f5e0269d6c2d27f07f1ca
DionneMaguire <dionnemaguire@yahoo.com>
2022-04-01 12:45:17 +0000

    add get sales function

--- run.py
+++ run.py
@@ -4,15 +4,18 @@ class Sales:
         self.quantity = quantity
         self.is_audited = is_audited
     def _str_(self):
-        return f'{self.quantity} {self.item}'
+        return f'{self.item} {self.quantity}'
 
-print("Please enter sales figures for the following items:")
-data_str1 = input("Enter sales figures for coke here:")
-print(f'The data provided is {data_str1}')
-sales_1 = Sales('coke', data_str1, False)
-sales = []
-sales.append(sales_1)
-print(sales_1._str_())
+def get_sales_data():
+    """
+    get sales figures from the user
+    """
+    print("Please enter sales figures for the following items:")
+    data_str1 = input("Enter sales figures for coke here:")
+    print(f'The data provided is {data_str1}')
+    sales_1 = Sales('coke', data_str1, False)
+    sales = []
+    sales.append(sales_1)
+    print(sales_1._str_())
 
-
-        
+get_sales_data()
