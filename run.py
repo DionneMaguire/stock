a55c9b0e80e242fc78d48aeee761f89c24739223
DionneMaguire <dionnemaguire@yahoo.com>
2022-04-01 14:32:01 +0000

    create instances of the stock class

--- run.py
+++ run.py
@@ -9,15 +9,40 @@ class Sales():
     def _str_(self):
         return f'{self.item} {self.quantity}'
 
-def get_sales_data():
+class Stock():
+    """
+    creates an instance of stock
+    """
+    def __init__(self, item, stock_quantity, reorder_level):
+        self.item = item
+        self.stock_quantity = stock_quantity
+        self.reorder_level = reorder_level
+    def _str_(self):
+        return f'{self.item} {self.stock_quantity} {self.reorder_level}'
+
+def create_stock_records(data):
+    """
+    creates stock instances for all items
+    """
+
+    stocks = []
+
+    for x in data:
+        stocksx = Stock(x,40,20)
+        stocks.append(stocksx)
+        print(stocksx._str_())
+
+    return stocks
+
+def get_sales_data(data):
     """
     get sales figures from the user
     """
-    items = ['coke','fanta','water']
+    
     sales =[]
     print("Please enter sales figures for the following items: ")
     
-    for x in items:
+    for x in data:
 
         data_str = input(f"Enter sales figures for {x} here:")
         print(f'The data provided for {x} is {data_str}')
@@ -28,5 +53,16 @@ def get_sales_data():
 
     return sales
 
-sales = get_sales_data()
+def update_stocks():
+    """
+    gets the stock value for each item and takes away the sales 
+    figure inputted by the user to give the current stock value
+    and write this back to the stock quantity in class
+    """
+    
+
+items = ['coke','fanta','water']
+stocks = create_stock_records(items)
+print(stocks[1].reorder_level)
+sales = get_sales_data(items)
 print(sales[0].item)
